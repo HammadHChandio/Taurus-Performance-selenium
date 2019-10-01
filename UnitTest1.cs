71361ce63ac042de1720854f3c21528c2e4e865e
HammadHChandio <hammad.hyder.chandio@outloo.com>
2019-10-01 11:12:12 +0100

    Adding dummy Nunit project

---
+++ UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TaurusPerformance
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        [SetUp]
+
+        public void setUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+        }
+
+        [TearDown]
+        public void close()
+        {
+            driver.Quit();
+        }
+    }
+}
